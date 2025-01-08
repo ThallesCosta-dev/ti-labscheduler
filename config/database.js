029dfd8010a0de455fb4bd76ff86ac3a67a60cd8
ThallesCosta-dev <costa.thalles71@gmail.com>
2025-01-08 00:42:09 -0300

    update commit

--- config/database.js
+++ config/database.js
@@ -4,11 +4,14 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.PORT || 3306,
+  database: 'u576483424_ti_labsched',
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool; 
